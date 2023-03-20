25391401f72ecf695e0cb73514357bce2919a9fe
YeltsinBL <gbl_leo_30@hotmail.com>
2023-03-20 00:05:21 -0500

    Iniciando con comentarios y tipos de datos con Type

---
+++ Hello.py
@@ -0,0 +1,28 @@
+# Primer Hola Mundo
+print("Hola Mundo Python")
+
+"""
+Comentario 
+en 
+varias 
+líneas con doble comilla
+"""
+'''
+Comentario 
+en 
+varias 
+líneas con comilla simple
+'''
+
+# Cadena de texto pueden ser con comilla doble o simple
+print("String con doble comilla")
+print("String con una comilla")
+
+# Type: indica el tipo de dato
+print(type("Hola Type")) # en este caso un String
+print(type(123)) # en este caso un Integer
+print(type(12.1)) # en este caso es un Float
+print(type(True)) # en este caso es un Booleano
+
+
+
